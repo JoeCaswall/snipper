b2e5220cee4148a83b667425bd8eab4c3d373e12
JoeCaswall <joe.caswall@gmail.com>
2024-01-24 16:32:13 +0000

    user works and password is hashed properly

--- app.py
+++ app.py
@@ -2,8 +2,9 @@ from flask import Flask, request, abort, make_response, jsonify
 import json
 
 from Models import db, User, Snippet
-from seed import seed, fernet
+from seed import seed, fernet, salt
 
+import bcrypt
 
 app = Flask(__name__)
 
@@ -49,15 +50,41 @@ def show_snippets_by_language(language):
     snippets = Snippet.query.all()
     for snippet in snippets:
         snippet.code = fernet.decrypt(snippet.code).decode()
-    correct_languages = Snippet.query.filter(Snippet.language == language.lower())
-    return jsonify([snippet.to_json() for snippet in correct_languages])
+    correct_languages = Snippet.query.filter(Snippet.language == language.lower()).one()
+    return [snippet.to_json() for snippet in correct_languages]
+
+# Placeholder user page
+@app.route("/snippets/users/")
+def user_page():
+    return "<h1>Send a request to login or signup</h1>"
 
 # Can create a new user
-@app.route("/snippets/signup/", methods="POST")
-def sign_up():
-    pass
+@app.route("/snippets/users/signup/", methods=["POST"])
+def user_sign_up():
+    request_data = request.get_json()
+    bytes = request_data["password"].encode("utf-8")
+    hashed = bcrypt.hashpw(bytes, salt)
+
+    new_user = User(email=request_data["email"], password=hashed)
+    db.session.add(new_user)
+    db.session.commit()
+    return "Signup successful!"
 
 # User can provide correct credentials and get a response containing their id and username
-app.route("/snippets/login/")
-def login():
-    pass
+@app.route("/snippets/users/login/", methods=["GET"])
+def user_login():
+    request_data = request.get_json()
+    target_email = request_data["email"]
+    correct_user = User.query.filter(User.email == target_email).one()
+    bytes = request_data["password"].encode("utf-8")
+    hashed = bcrypt.hashpw(bytes, salt)
+    stored_hash = correct_user.password
+    if hashed == stored_hash:
+        data_to_send = jsonify({"id": correct_user.id, "email": correct_user.email})
+        response = make_response(data_to_send)
+        response.status_code = 200
+        return response
+    else:
+        response = make_response("<h1>Incorrect username or password</h1>")
+        response.status_code = 401
+        return response
